64705d56648e285fde49072c661b8f5c5d9b212a
Binispeter <binispeter@gmail.com>
2025-12-03 11:23:31 +0200

    Made pathResolver methods static

--- backend/pathResolver.py
+++ backend/pathResolver.py
@@ -2,11 +2,10 @@ import os
 import sys
 
 class PathResolver:
-    def __init__(self):
-        self.script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+    @staticmethod
+    def get_project_path():
+        return os.path.dirname(os.path.abspath(sys.argv[0])) + '\\'
 
-    def get_project_path(self):
-        return self.script_directory + '\\'
-
-    def get_db_path(self):
-        return self.get_project_path() + 'data.db'
+    @staticmethod
+    def get_db_path():
+        return PathResolver.get_project_path() + 'data.db'
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ sys.dont_write_bytecode = True #testing
 sys.path.insert(0, './backend')
 from backend.pathResolver import PathResolver
 
-def main(self):
-    print(PathResolver.get_db_path(self))
+def main():
+    print(PathResolver.get_db_path())
 
 if __name__ == '__main__':
-    main(self)
+    main()
