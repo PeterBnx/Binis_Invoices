50100303ccb9089f970fae01bbf076e8716f660e
Peter <binispeter@gmail.com>
2025-12-10 14:40:14 +0200

    Shared class fixed

--- backend/Shared.py
+++ backend/Shared.py
@@ -9,6 +9,10 @@ from backend.pathResolver import PathResolver
 class Shared:
 
     def __init__(self):
+        self.emp_login_url = f"https://www.emporiorologion.gr/admin/controlloaccesso.php"
+        self.emp_orders_page_url = f"https://www.emporiorologion.gr/admin/ordini2.php?statoordine=-1&nome=&ordine=&cliente=&referenza=&ups=&datachiusura=&Payment=0&annullato=0&nazione_ordine=-1&ordine_pronto=2&warning=2&marca=-1&primo_ordine=2&pagato=2&API=2&chiavi_in_mano=2&vendita_privata=2&datadal=&dataal="
+        self.cis_login_url = f"https://live.livecis.gr/live/default.aspx"
+        self.cis_items_url = f"https://live.livecis.gr/live/Materials.aspx?tp=%C5%DF%E4%EF%F2"
         self.load_credentials()
         self.session = Session()
 
@@ -32,7 +36,7 @@ class Shared:
         self.session = Session()
 
     def _load_cis_tokens(self):
-        r = self.session.get("https://live.livecis.gr/live/default.aspx")
+        r = self.session.get(self.cis_login_url)
         soup = BeautifulSoup(r.text, "html.parser")
 
         return {
@@ -55,13 +59,11 @@ class Shared:
         }
 
         self.session.post(
-            "https://live.livecis.gr/live/default.aspx",
+            self.cis_login_url,
             data=login_payload
         )
 
-        items_page = self.session.get(
-            "https://live.livecis.gr/live/Materials.aspx?tp=%C5%DF%E4%EF%F2"
-        )
+        items_page = self.session.get(self.cis_items_url)
         soup = BeautifulSoup(items_page.text, "html.parser")
 
         def get_value(name):
@@ -76,10 +78,7 @@ class Shared:
             "__EVENTVALIDATION": get_value("__EVENTVALIDATION")
         }
 
-        export_response = self.session.post(
-            "https://live.livecis.gr/live/Materials.aspx?tp=%C5%DF%E4%EF%F2",
-            data=export_payload
-        )
+        export_response = self.session.post(self.cis_items_url, data=export_payload)
 
         try:
             df = pd.read_excel(io.BytesIO(export_response.content))
@@ -100,24 +99,17 @@ class Shared:
 
     def get_all_orders(self):
         self.reset_session()
-        login_payload = {
+        self.emp_payload = {
             "login": self.emp_name,
             "password": self.emp_passwd
         }
 
         self.session.post(
-            "https://www.emporiorologion.gr/admin/controlloaccesso.php",
-            data=login_payload
+            self.emp_login_url,
+            data=self.emp_payload
         )
 
-        response = self.session.get(
-            "https://www.emporiorologion.gr/admin/ordini2.php"
-            "?statoordine=-1&nome=&ordine=&cliente=&referenza=&ups=&"
-            "datachiusura=&Payment=0&annullato=0&nazione_ordine=-1&"
-            "ordine_pronto=2&warning=2&marca=-1&primo_ordine=2&"
-            "pagato=2&API=2&chiavi_in_mano=2&vendita_privata=2&"
-            "datadal=&dataal="
-        )
+        response = self.session.get(self.emp_orders_page_url)
 
         soup = BeautifulSoup(response.text, "html.parser")
 
